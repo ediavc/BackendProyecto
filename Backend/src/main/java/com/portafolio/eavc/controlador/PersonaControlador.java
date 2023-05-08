250f128f283322075158fe057060669e73819a93
Edilin Valeri <edilinvaleri3@gmail.com>
2023-05-08 01:59:14 -0300

    deploy3

--- Backend/src/main/java/com/portafolio/eavc/controlador/PersonaControlador.java
+++ Backend/src/main/java/com/portafolio/eavc/controlador/PersonaControlador.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://frontendeavc.web.app"})
 public class PersonaControlador {
     @Autowired
     ImpPersonaServicio personaService;
